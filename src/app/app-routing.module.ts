56587666b79c81ffd0d729ede7454cfeffe3f770
it191506 <it191506@fhstp.ac.at>
2020-03-20 16:15:35 +0100

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)},
+  {
+    path: 'quiz',
+    loadChildren: () => import('./pages/quiz/quiz.module').then( m => m.QuizPageModule)
+  },
+  {
+    path: 'question-list',
+    loadChildren: () => import('./pages/question-list/question-list.module').then( m => m.QuestionListPageModule)
+  },
+  {
+    path: 'question',
+    loadChildren: () => import('./pages/question/question.module').then( m => m.QuestionPageModule)
+  },
 ];
 
 @NgModule({
